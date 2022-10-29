87fbea3ae0be6b74f878854f3a64425a71c97b9c
robogf <111620315+robogf@users.noreply.github.com>
2022-10-29 15:43:56 -0700

    created table and db for mysql tables

---
+++ db/schema.sql
@@ -0,0 +1,22 @@
+DROP DATABASE IF EXISTS companyDB;
+CREATE DATABASE companyDB;
+
+use companyDB;
+
+CREATE TABLE departments (
+    id INT AUTO_INCREMENT  NOT NULL,
+    department_name 
+)
+
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT NOT NULL,
+    role_title  
+
+)
+
+CREATE employees (
+    id INT NOT NULL, 
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+
+)
