00a91836cc026114d58595685dad908292bafd1d
robogf <111620315+robogf@users.noreply.github.com>
2022-10-30 16:53:21 -0700

    finished required schema.sql file

--- db/schema.sql
+++ db/schema.sql
@@ -4,19 +4,24 @@ CREATE DATABASE companyDB;
 use companyDB;
 
 CREATE TABLE departments (
-    id INT AUTO_INCREMENT  NOT NULL,
-    department_name 
-)
+    id INT AUTO_INCREMENT  PRIMARY KEY,
+    department_name VARCHAR(30)
+);
 
 CREATE TABLE roles (
-    id INT AUTO_INCREMENT NOT NULL,
-    role_title  
-
-)
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    role_title VARCHAR(30),
+    salary DECIMAL,
+    departments_id INT,
+    FOREIGN KEY(departments_id) REFERENCES departments(id)
+);
 
 CREATE employees (
-    id INT NOT NULL, 
+    id INT AUTO_INCREMENT PRIMARY KEY, 
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-
-)
+    employee_id INT,
+    FOREIGN key (employee_id) REFERENCES roles(id)
+    manager_id INT,
+    FOREIGN KEY (manager_id) REFERENCES employees(id)
+);
