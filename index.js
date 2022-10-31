d3778e329bb4c4d1243fbaa37a281d67a1beef27
robogf <111620315+robogf@users.noreply.github.com>
2022-10-31 06:43:38 -0700

    connected our DB and started inquirer prompt

--- index.js
+++ index.js
@@ -3,3 +3,65 @@ const mysql = require('mysql2');
 const cTable = require('console.table');
 
 
+var db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+
+    password: 'angel123',
+    database: 'companyDB'
+},
+console.log('we made it into database!')
+)
+
+db,connect(function (err) {
+    if (err) throw err; 
+})
+
+function manageCompany() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "userOptions",
+            message: "What would you like to do?",
+            choices: ["View All Employees","Add Employee","Update Employee Role","View All Roles",
+        "Add Role","View All Departments","Add Department","Quit"]
+        }
+    ])
+    .then(function(answer) {
+        switch (answer.userOptions) {
+            case "View All Employees":
+            viewEmployees();
+            break;
+
+            case "Add Employee":
+            addEmployee();
+            break;
+
+            case "Update Employee Role":
+            updateEmployee();
+            break;
+
+            case "View All Roles":
+            viewRoles();
+            break;
+
+            case "Add Role":
+            addRole();
+            break;
+
+            case "View All Departments":
+            viewDepartments();
+            break;
+
+            case "Add Department":
+            addDepartment();
+            break;
+
+            case "Quit":
+            exit();
+            break;
+
+
+        }
+    })
+}
