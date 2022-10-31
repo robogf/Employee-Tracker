f0f6ea7dbcf395730919c9773b60a1ebcd380370
robogf <111620315+robogf@users.noreply.github.com>
2022-10-31 00:32:51 -0700

    inserted data onto our first two tables

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,18 @@
-INSERT INTO departments (
-    
-)
+INSERT INTO departments (department_name) 
+VALUES  ("Engineering"),
+        ("Finance"),
+        ("Legal"),
+        ("Sales");
+
+INSERT INTO roles (role_title,salary,departments_id)
+VALUES  ("Lead Engineer","180000",1),
+        ("Software Engineer", "130000",1),
+        ("Sales Lead", "100000",2),
+        ("Salesperson","80000",2),
+        ("Account Manager", "160000",3),
+        ("Accountant", "120000",3),
+        ("Legal Team Lead", "250000",4),
+        ("Lawyer", "150000",4);
+
+
+INSERT INTO employees ()
